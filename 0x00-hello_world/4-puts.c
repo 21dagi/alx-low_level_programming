620965de00bde2dab7e8e0fe4e6315f76b8a4f98
21dagi <21dagmawinegussie@gmail.com>
2023-09-10 12:08:27 -0700

    g

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program.
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
