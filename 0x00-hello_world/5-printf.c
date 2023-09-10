dc7b6a77ea20308fc8fe1dcc97b5c24980131116
21dagi <21dagmawinegussie@gmail.com>
2023-09-10 12:32:48 -0700

    a C program that print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program.
+  * Return: Always 0 (Success)
+  */
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
