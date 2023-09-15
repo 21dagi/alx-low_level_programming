2e1daa340f8045ed65c77303b1cdfc0854f8496c
21dagi <21dagmawinegussie@gmail.com>
2023-09-15 03:11:22 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* printf: prints val
+* Return: 0
+* '\\o/\n': hy kyga
+*/
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+printf("Infinite loop avoided! \\o/\n");
+/**
+*i = 0; - hey
+*while (i < 10) - hey
+*{- hey
+*putchar(i); - hey
+*} - hey
+*/
+
+return (0);
+}
