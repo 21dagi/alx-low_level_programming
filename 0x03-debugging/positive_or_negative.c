1f2d8dd4fdfc5119de2fd6e25115275ddfd95aeb
21dagi <21dagmawinegussie@gmail.com>
2023-09-15 01:41:19 -0700

    0. Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
+/**
+ * positive_or_negative - prints the polarity
+ * Return - ....
+ * @i: value
+ */
+
 void positive_or_negative(int i)
 {
-        if (i > 0)
-        {
-                printf("%dd is positive\n", i);
-        }
-        else if (i == 0)
-        {
-                printf("%d is zero\n", i);
-        }
-        else
-        {
-                printf("%d is negative\n", i);
-        }
+if (i > 0)
+{
+printf("%dd is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
 }
