9971169db2e0e559cef025f9c39c566f8892cadf
21dagi <21dagmawinegussie@gmail.com>
2023-09-18 15:02:55 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
-  * _puts - prints a string.
-  * @str: string to print
-  * Return: length
-  */
-
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
 void _puts(char *str)
 {
-while (*str != '\0')
-{
-_putchar(*str++);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
-_putchar("\n");
